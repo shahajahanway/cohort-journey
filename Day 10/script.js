f71f7ef7c28c9b5ece6c536e57edc9dcbac9eeec
Shahajahan Sheikh <167621740+shahajahanway@users.noreply.github.com>
2025-10-07 23:18:51 +0530

    Delete Day 10/script.js

--- Day 10/script.js
+++
@@ -1,9 +0,0 @@
-
-  const toggle = document.querySelector('.nav-toggle'); // Toggle button select
-  const menu = document.getElementById('primary-menu'); // Menu select
-
-  toggle.addEventListener('click', () => { // Click event
-    const expanded = toggle.getAttribute('aria-expanded') === 'true'; // Check open/close state
-    toggle.setAttribute('aria-expanded', !expanded); // Update ARIA attribute for accessibility
-    menu.style.display = expanded ? 'none' : 'flex'; // Show/Hide menu
-  });
